9de36633e1424a13025af4accd14218c4e3478bc
Timur Malikin <timur.malikin@gmail.com>
2016-07-31 23:34:25 +0300

    phpunit settings

---
+++ src/linter.php
@@ -0,0 +1,7 @@
+<?php
+namespace Linter;
+
+function lint()
+{
+    return true;
+}
---
+++ tests/LintTest.php
@@ -0,0 +1,10 @@
+<?php
+use function Linter\lint;
+
+class LintTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFileConvert()
+    {
+        $this->assertTrue(lint());
+    }
+}
