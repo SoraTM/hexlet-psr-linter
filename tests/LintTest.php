77dd65f677f79c33cc6f93ccacd6f82a9e871250
Timur Malikin <timur.malikin@gmail.com>
2016-08-03 14:32:08 +0300

    Revert "Stage2"

--- tests/LintTest.php
+++ tests/LintTest.php
@@ -1,50 +1,12 @@
 <?php
-
 namespace Linter;
+
+use function \Linter\lint;
 
 class LintTest extends \PHPUnit_Framework_TestCase
 {
-    private $functionErrors = [];
-    private $classErrors = [];
-    
-    public function setUp()
+    public function testFileConvert()
     {
-        $this->functionErrors = [
-            [
-                'error:',
-                'Function name MUST be in camelCase',
-                'NotCamelCase',
-            ],
-            [
-                'error:',
-                'Function name MUST NOT include underscores',
-                'with_under_scores',
-            ],
-        ];
-    }
-    
-    public function testValidFunctionName()
-    {
-        $this->assertTrue(\Linter\isFunctionNameCamelCase('camelCase'));
-        $this->assertFalse(\Linter\isFunctionNameCamelCase('NotCamelCase'));
-        $this->assertTrue(\Linter\isFunctionNameWithUnderScores('with_under_scores'));
-        $this->assertFalse(\Linter\isFunctionNameWithUnderScores('withunderscores'));
-    }
-    
-    public function testCheckFunctionName()
-    {
-        $this->assertEquals(
-            'Function name MUST be in camelCase',
-            \Linter\checkFunctionName('NotCamelCase')
-        );
-        $this->assertEquals(
-            'Function name MUST NOT include underscores',
-            \Linter\checkFunctionName('with_under_scores')
-        );
-    }
-    
-    public function testCheckFunctionNames()
-    {
-        $this->assertEquals($this->functionErrors, \Linter\checkFunctions(['NotCamelCase', 'with_under_scores']));
+        $this->assertTrue(lint());
     }
 }
